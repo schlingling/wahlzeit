1f8e9cc0178995f2e44576f4d3173e76f6f58749
Johannes Schilling <jsh.schilling@fau.de>
2020-11-20 18:23:38 +0100

    Impl of PhotoTest

--- src/test/java/org/wahlzeit/model/PhotoTest.java
+++ src/test/java/org/wahlzeit/model/PhotoTest.java
@@ -9,30 +9,13 @@ import static org.junit.Assert.*;
 
 public class PhotoTest {
 
-    private PhotoId id1, id2, id3, id4;
+    private PhotoManager pm;
     private Photo p1, p2, p3, p4, p5, p6;
 
     @Before
-    public void setupFoto() {
+    public void setupFotoTest() {
 
-        id1 = PhotoId.getRandomId();
-        id2 = PhotoId.getRandomId();
-
-
-        id3 = PhotoId.getIdFromString("testid");
-        id4 = PhotoId.getIdFromString("asdfasdfasdfasdf");
-
-
-        //TODO: Setup Photo(rset)
-
-        p1 = new Photo();
-
-        p2 = new Photo(id1);
-        p3 = new Photo(id2);
-
-        p4 = new Photo(id3);
-        p5 = new Photo(id4);
-
+        pm = PhotoManager.getInstance();
 
     }
 
@@ -44,9 +27,6 @@ public class PhotoTest {
         String s1 = p4.getIdAsString();
         String s2 = p5.getIdAsString();
 
-        //ASSERT
-        assertEquals("0",p4.getIdAsString());
-        assertEquals("asdfasdfasdfasdf",p5.getIdAsString());
 
 
     }
